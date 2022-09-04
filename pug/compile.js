149e3562ebafe0021072d17fe69eaa80589536dc
Christian Kozalla <christian.kozalla@gmail.com>
2022-09-04 22:50:50 +0200

    Pug Example #2

--- pug/compile.js
+++ pug/compile.js
@@ -1,6 +1,9 @@
-const pug = require("pug");
-const fs = require("fs");
-const path = require("path");
+const pug = require('pug');
+const fs = require('fs');
+const path = require('path');
 
-const indexCompiler = pug.compileFile("index.pug");
-fs.writeFileSync(path.join(__dirname, "index.html"), indexCompiler({ aVariable: "Christian" }));
+const indexCompiler = pug.compileFile('index.pug');
+fs.writeFileSync(
+  path.join(__dirname, 'html', 'index.html'),
+  indexCompiler({ aVariable: 'Christian' })
+);
